e1e85d121fe15de8df32b4ccc2df5cc689b8f30f
Alex Myltsev <alexander.myltsev@gmail.com>
2025-10-27 17:58:51 +0300

    Publish to github pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://alexander-myltsev.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/buhler-sanskrit-book/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'alexander-myltsev',
+  projectName: 'buhler-sanskrit-book',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
 
